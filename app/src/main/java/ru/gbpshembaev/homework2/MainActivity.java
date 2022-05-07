649952454e8564b9ce9295f3ad789b9fa50ac993
11051997amantay <104579592+11051997amantay@users.noreply.github.com>
2022-05-08 02:19:51 +0600

    Initial commit

---
+++ app/src/main/java/ru/gbpshembaev/homework2/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.gbpshembaev.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  int  orientation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        orientation = Configuration.ORIENTATION_PORTRAIT;
+    }
+}
